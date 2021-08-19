c89449a057248f47ee98ba365183eb14a5b35dfa
yaroamaro219 <yaroamari@gmail.com>
2021-08-19 09:24:11 -0400

    Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router'
+import About from './components/About'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        taste budds
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/about' render={(props) => (
+            <About 
+            />
+          )}/>
+           <Route exact path='/' render={(props) => (
+            <Home
+            />
+          )}/>
+        </Switch>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <h1>Taste Budds</h1>
+        <a href='/about'>About</a>
+            </div>
+        )
+    }
+}
